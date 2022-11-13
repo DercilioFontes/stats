abe2c729220664d7f8dd11e2db43db61b259eb50
Dercilio Fontes <dercilioafontes@gmail.com>
2022-11-12 20:17:32 -0500

    Adjusted Copyright

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Stack } from 'rsuite';
 
+const year = new Date().getFullYear();
+
 const Copyright = () => {
   return (
     <Stack className="copyright" justifyContent="center" style={{ height: 40, marginTop: 20 }}>
       <div className="container">
         <p>
-          © 2022, Made with ❤️ by{' '}
+          © {year}, Made with ❤️ by{' '}
+          <a href="https://www.linkedin.com/in/derciliofontes/" target="_blank" rel="noreferrer">
+            Dercilio Fontes
+          </a>{' '}
+          and{' '}
           <a href="https://github.com/rsuite/rsuite" target="_blank" rel="noreferrer">
             RSUITE
-          </a>
+          </a>{' '}
+          (forked template)
         </p>
       </div>
     </Stack>
