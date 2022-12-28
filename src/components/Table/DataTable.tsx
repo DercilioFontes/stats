905040653520027212a5aebcac578357f8fc94fb
Dercilio Fontes <dercilioafontes@gmail.com>
2022-12-28 18:23:27 -0500

    Added .table-header class

--- src/components/Table/DataTable.tsx
+++ src/components/Table/DataTable.tsx
@@ -15,22 +15,22 @@ const DataTable = ({ data }: { data: TableData[] }) => {
     <Panel className="card" header="Table">
       <Table height={300} data={data} rowKey="label">
         <Column flexGrow={1} minWidth={100}>
-          <HeaderCell>Classes</HeaderCell>
+          <HeaderCell className="table-header">Classes</HeaderCell>
           <Cell dataKey="label"></Cell>
         </Column>
 
         <Column width={200}>
-          <HeaderCell>Frequency</HeaderCell>
+          <HeaderCell className="table-header">Frequency</HeaderCell>
           <Cell dataKey="frequency" />
         </Column>
 
         <Column width={200}>
-          <HeaderCell>Relative frequency (%)</HeaderCell>
+          <HeaderCell className="table-header">Relative frequency (%)</HeaderCell>
           <Cell dataKey="relativeFrequency" />
         </Column>
 
         <Column width={200}>
-          <HeaderCell>Cumulative frequency</HeaderCell>
+          <HeaderCell className="table-header">Cumulative frequency</HeaderCell>
           <Cell dataKey="cumulativeFrequency" />
         </Column>
       </Table>
