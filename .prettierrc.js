5a79c73e7c9e0a2093f914ed1564d341919017d6
DercilioFontes <dercilioafontes@gmail.com>
2022-12-29 14:28:26 -0500

    Added StrictMode

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,6 @@ module.exports = {
   printWidth: 100,
   tabWidth: 2,
   singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'none'
 };
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { IntlProvider } from 'react-intl';
 import { CustomProvider } from 'rsuite';
-import enUS from 'rsuite/locales/en_GB';
+import enUS from 'rsuite/locales/en_US';
 import locales from './locales';
 import Frame from './components/Frame';
 import HistogramPage from './pages/histogram';
